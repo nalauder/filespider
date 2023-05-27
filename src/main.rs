aa76f48dc52b9615d8c340a257db1617b6455f3d
Nick Lauder <nick.lauder@outlook.com>
2023-05-27 18:34:42 +1200

    Updated type usage and error handling

--- src/main.rs
+++ src/main.rs
@@ -16,63 +16,53 @@ fn pattern_builder(
     Regex::new(&search_pattern).unwrap()
 }
 
-fn load_file(file_path: &String) -> Result<String, std::io::Error> {
+fn load_file(file_path: &PathBuf) -> Result<String, std::io::Error> {
     let file = fs::read_to_string(file_path)?;
     Ok(file)
 }
 
-fn parse_path(path: &String, files: &mut Vec<std::path::PathBuf>) {
-    let file = fs::metadata(path);
-    if file.is_ok() {
-        if file.unwrap().is_file() {
-            files.push(fs::canonicalize(path).unwrap());
-        } else {       
-            match fs::read_dir(path) {
-                Ok(paths) => {
-                    for path_obj in paths {
-                        parse_path(
-                            &path_obj.unwrap().path().to_str().unwrap().to_string(),
-                            files,
-                        )
-                    }
-                },
-                Err(_) => println!("Error opening file - {}", path),
-            }
-        }
+fn identify_files_local(path: PathBuf) -> Result<Vec<PathBuf>, std::io::Error> {
+    let mut files: Vec<PathBuf> = Vec::new();
+    let file: fs::Metadata = fs::metadata(&path)?;
+
+    if file.is_file() {
+        files.push(fs::canonicalize(path)?);
     } else {
-        println!("File not found - {}", path);   
+        let paths: fs::ReadDir = fs::read_dir(path)?;
+        for path_obj in paths {
+            let mut new_files: Vec<PathBuf> = identify_files_local(path_obj.unwrap().path())?;
+            files.append(&mut new_files);
+        }
     }
+
+    Ok(files)
 }
 
-fn match_in_file(file: PathBuf, regex_pattern: &Regex, print_line: bool) {
-    let path_name: String = file.into_os_string().into_string().unwrap();
-    
-    match load_file(&path_name) {
-        Ok(file_contents) => {
-            for (i, l) in file_contents.lines().enumerate() {
-                let matched: Option<regex::Match> = regex_pattern.find(l);
-        
-                if matched.is_some() {
-                    let match_obj = matched.unwrap();
-                    if print_line {
-                        println!("{}:{} - {}", path_name, i+1, match_obj.as_str());
-                    } else {
-                        println!("{} - {}", path_name, match_obj.as_str());
-                    }
-                    
+fn match_in_file(file: &PathBuf, regex_pattern: &Regex, print_line_num: bool) {
+    let path_name = file.to_str().unwrap();
+
+    let file_contents = load_file(&file);
+
+    if file_contents.is_ok() {
+        for (i, l) in file_contents.unwrap().lines().enumerate() {
+            let matched: Option<regex::Match> = regex_pattern.find(l);
+
+            if matched.is_some() {
+                let match_obj = matched.unwrap();
+                if print_line_num {
+                    println!("{}:{} - {}", path_name, i + 1, match_obj.as_str());
+                } else {
+                    println!("{} - {}", path_name, match_obj.as_str());
                 }
             }
-        },
-        Err(_) => return,
+        }
     }
-
-    
 }
 
 #[derive(Parser)]
 struct Parameters {
     #[arg(short, long)]
-    file_path: String,
+    file_path: PathBuf,
     #[arg(short, long)]
     pattern: String,
 
@@ -99,10 +89,11 @@ fn main() {
         args.ignore_case,
     );
 
-    let mut files: Vec<std::path::PathBuf> = Vec::new();
-    parse_path(&args.file_path, &mut files);
+    let files = identify_files_local(args.file_path);
 
-    for file in files {
-        match_in_file(file, &regex_pattern, args.print_line_number);
+    if files.is_ok() {
+        for file in files.unwrap() {
+            match_in_file(&file, &regex_pattern, args.print_line_number);
+        }
     }
 }
